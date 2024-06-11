aa40510cbd3e15f2ff9ad16a0e89f9140440a76b
awash <Emerald190!!>
2024-06-11 11:28:46 -0500

    add awash-brewery-gateway

---
+++ src/main/java/com/sfg/gateway/AwashBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwashBreweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwashBreweryGatewayApplication.class, args);
+	}
+
+}
